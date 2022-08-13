c43f513c7e58042132850c320042bea0d0ec55b7
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-08-13 20:14:20 +0500

    6 change

--- src/views/EggploreContentThree.vue
+++ src/views/EggploreContentThree.vue
@@ -92,97 +92,24 @@
         </div>
       </div>
       <div class="direction-form">
-        <h5>Direction Form</h5>
-        <div class="direction-container" ref="containterDiv">
-          <div class="own-location"
-          :class="isPathDirection ? 'own-location-active' : 'own-location-deactive'"
-          >
-            <div class="location-title">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="currentcolor"
-                xmlns="http://www.w3.org/2000/svg"
-                class="own-location-svg"
-              >
-                <path
-                  d="M0.768857 4.04917L14.596 0.098548C15.3861 -0.0989828 16.0775 0.592375 15.88 1.3825L11.9294 15.2097C11.6331 16.0985 10.4479 16.1973 10.0528 15.3084L7.28738 8.78991L0.768857 6.02447C-0.218798 5.53065 -0.120031 4.34546 0.768857 4.04917Z"
-                  fill="currentcolor"
-                />
-              </svg>
-              {{ own_destination }}
-            </div>
-          </div>
-
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="currentcolor"
-            xmlns="http://www.w3.org/2000/svg"
-            class="change-path-svg"
-            @click="changePath"
-          >
-            <path
-              d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z"
-              fill="currentcolor"
-            />
-            <path
-              d="M16.7794 20.4062H19.0938C19.508 20.4062 19.8438 20.742 19.8438 21.1562C19.8438 21.5705 19.508 21.9062 19.0938 21.9062H14.9688C14.5545 21.9062 14.2188 21.5705 14.2188 21.1562V17.0312C14.2188 16.617 14.5545 16.2812 14.9688 16.2812C15.383 16.2812 15.7188 16.617 15.7188 17.0312V19.3456L20.6259 14.4384C20.9188 14.1455 21.3937 14.1455 21.6866 14.4384C21.9795 14.7313 21.9795 15.2062 21.6866 15.4991L16.7794 20.4062ZM15.2206 11.5938H12.9062C12.492 11.5938 12.1562 11.258 12.1562 10.8438C12.1562 10.4295 12.492 10.0938 12.9062 10.0938H17.0312C17.4455 10.0938 17.7812 10.4295 17.7812 10.8438V14.9688C17.7812 15.383 17.4455 15.7188 17.0312 15.7188C16.617 15.7188 16.2812 15.383 16.2812 14.9688V12.6544L11.3741 17.5616C11.0812 17.8545 10.6063 17.8545 10.3134 17.5616C10.0205 17.2687 10.0205 16.7938 10.3134 16.5009L15.2206 11.5938Z"
-              fill="currentcolor"
-              class="change-svg-second"
-            />
-          </svg>
-
-          <div class="search-location"
-          :class="isPathDirection ? 'search-location-active' : 'search-location-deactive'"
-          >
-            <svg
-              width="16"
-              height="18"
-              viewBox="0 0 16 18"
-              fill="currentcolor"
-              xmlns="http://www.w3.org/2000/svg"
-              class="search-location-svg"
-            >
-              <path
-                d="M8.00344 0C4.55996 0 0.959961 2.66087 0.959961 7.04348C0.959961 11.1913 7.22083 17.5304 7.45561 17.7652C7.61213 17.9217 7.76866 18 8.00344 18C8.23822 18 8.39474 17.9217 8.55126 17.7652C8.78605 17.5304 15.0469 11.1913 15.0469 7.04348C15.0469 2.66087 11.4469 0 8.00344 0ZM8.00344 9.3913C6.673 9.3913 5.65561 8.37391 5.65561 7.04348C5.65561 5.71304 6.673 4.69565 8.00344 4.69565C9.33387 4.69565 10.3513 5.71304 10.3513 7.04348C10.3513 8.37391 9.33387 9.3913 8.00344 9.3913Z"
-                fill="currentcolor"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_72967_235"
-                  x1="8.00344"
-                  y1="-9"
-                  x2="-9.46901"
-                  y2="4.67409"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#BD7AE3" />
-                  <stop offset="1" stop-color="#8461C9" />
-                </linearGradient>
-              </defs>
-            </svg>
-            <input
-              type="text"
-              class="input-location"
-              placeholder="Enter destination..."
-            />
-          </div>
-        </div>
+        <eggplore-location/>
       </div>
-      <div class="date-picker"></div>
+      <div class="date-picker">
+        <eggplore-calendar/>
+      </div>
       <div class="text-box"></div>
     </div>
   </div>
 </template>
 <script>
+import EggploreLocation from './eggplore-three/EggploreLocation.vue'
+import EggploreCalendar from './eggplore-three/EggploreCalendar.vue';
 export default {
   name: "search-app",
+  components:{EggploreLocation, EggploreCalendar},
   data() {
     return {
-        isPathDirection: false,
+      
       own_destination: "Your location",
       search_input: null,
       input_one_found: null,
@@ -203,31 +130,14 @@ export default {
       ],
     };
   },
-  mounted() {
-    
-  },
+  mounted() {},
   methods: {
-    changePath(){
-        // this.isPathDirection = !this.isPathDirection
-
-        let parentNode = this.$refs.containterDiv;
-        let firstChildEl = parentNode.firstChild;
-        let lastChildEl = parentNode.lastChild;
-        lastChildEl.style.marginTop = 0;
-        lastChildEl.style.borderTop = 'none';
-
-        firstChildEl.style.marginTop = 15
-        firstChildEl.style.borderTop = '1px dashed #ecebed'
-        console.log(this.$refs)
-
-        parentNode.append(firstChildEl)
-        parentNode.prepend(lastChildEl)
-    },
+    
     findRegion(e) {
       let input_value = e.target.value;
-      this.region = this.regions.filter(item => {
+      this.region = this.regions.filter((item) => {
         item = item.toLowerCase();
-        return item.includes(input_value.toLowerCase())
+        return item.includes(input_value.toLowerCase());
       });
       // this.regions.forEach((element) => {
       //   // let search_reg = element.toLowerCase
@@ -237,6 +147,7 @@ export default {
       // });
       console.log(this.region);
     },
+    
     deleteSearchInput() {
       this.search_input = null;
     },
@@ -247,230 +158,9 @@ export default {
 };
 </script>
 <style>
-.search-container {
-  margin: 60px 0 0 120px;
-}
-.search-container .search-content {
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-}
-.search-container .search-content .search-bar {
-  display: flex;
-  flex-direction: column;
-  width: 24%;
-}
-
-.search-content .search-bar h5 {
-  text-transform: uppercase;
-  display: inline-block;
-  margin: 0;
-  font-size: 1rem;
-  line-height: 24px;
-}
-.search-content .search-bar .search-bar-content {
-  width: 100%;
-  position: relative;
-  margin-top: 20px;
-}
-.search-content .search-bar .search-bar-content .search-input {
-  width: 90%;
-  height: 42px;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
-  padding: 10px 15px 10px 40px;
-  box-sizing: border-box;
-  color: #d0c9d6;
-  font-size: 0.95rem;
-  line-height: 20px;
-}
-.search-content .search-bar .search-bar-content .search-input:focus {
-  outline: none;
-  border: 1px solid #ecebed;
-  color: #3f3356;
-}
-.search-content .search-bar .search-bar-content .search-svg-active {
-  position: absolute;
-  left: 10px;
-  top: 10px;
-}
-.search-content .search-bar .search-bar-content .search-svg-deactive {
-  position: absolute;
-  left: 10px;
-  top: 10px;
-  color: #d0c9d6;
-}
-@keyframes Search {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(0.5);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-.search-content .search-bar .search-bar-content .delete-search-active {
-  position: absolute;
-  right: 35px;
-  top: 15px;
-  animation: Search 1s linear;
-  cursor: pointer;
-}
-.search-content .search-bar .search-bar-content .delete-search-deactive {
-  position: absolute;
-  right: 35px;
-  top: 15px;
-  height: 0;
-}
-.search-bar .search-one-bar {
-  width: 100%;
-  margin-top: 20px;
-  position: relative;
-}
-.search-bar .search-one-bar .search-input-one {
-  width: 90%;
-  height: 42px;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
-  padding: 10px 15px 10px 35px;
-  box-sizing: border-box;
-  font-size: 0.95rem;
-  line-height: 20px;
-}
-.search-bar .search-one-bar .search-input-one:focus {
-  outline: none;
-  border: 1px solid #ecebed;
-}
-.search-bar .search-one-bar .one-active-search {
-  position: absolute;
-  left: 10px;
-  top: 10px;
-}
-.search-bar .search-one-bar .one-deactive-search {
-  position: absolute;
-  left: 10px;
-  top: 10px;
-  color: #d0c9d6;
-}
-.search-bar .search-one-bar .one-active-delete {
-  position: absolute;
-  right: 35px;
-  top: 15px;
-  animation: Search 1s linear;
-  cursor: pointer;
-}
-.search-bar .search-one-bar .one-deactive-delete {
-  position: absolute;
-  right: 35px;
-  top: 15px;
-  height: 0;
-}
-.search-bar .search-one-bar .wrap-one-active {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  height: auto;
-  background: #ffffff;
-  width: 90%;
-  border-radius: 5px;
-  margin-top: 5px;
-  animation: Search 1s;
-}
-.search-bar .search-one-bar .wrap-one-active span {
-  cursor: pointer;
-  padding-top: 10px;
-  box-sizing: border-box;
-  box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
-}
-.search-bar .search-one-bar .wrap-one-deactive {
-  height: 0;
-  overflow: hidden;
-}
-
-.search-container .search-content .direction-form {
+.search-container .search-content .date-picker{
   width: 24%;
   margin-left: 20px;
-  display: flex;
-  flex-direction: column;
 }
-.search-content .direction-form h5 {
-  display: inline-block;
-  margin: 0;
-  font-size: 1rem;
-  text-transform: uppercase;
-  line-height: 24px;
-}
-.search-content .direction-form .direction-container {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-  background: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
-  padding: 15px 45px 15px 45px;
-  box-sizing: border-box;
-  position: relative;
-}
-.direction-form .direction-container .own-location {
-  position: relative;
-  width: 100%;
-}
-.direction-form .direction-container .own-location-active{
-    position: absolute;
-    bottom: 10px;
-}
-.direction-container .own-location .location-title {
-  width: 100%;
-  margin: auto;
-  color: #6979f8;
-  font-size: 0.95rem;
-  line-height: 20px;
-}
-.direction-container .own-location .location-title .own-location-svg {
-  position: absolute;
-  left: -30px;
-  top: 5px;
-}
-.direction-container .search-location {
-  width: 100%;
-  margin-top: 15px;
-  border-top: 1px dashed #ecebed;
-  position: relative;
-}
-.direction-container .search-location-active{
-    position: absolute;
-    top: 10px;
-}
-.direction-container .search-location .input-location {
-  width: 90%;
-  height: 36px;
-  border: none;
-  border-radius: 5px;
-  font-size: 0.95rem;
-  line-height: 20px;
-}
-.direction-container .search-location .input-location:focus {
-  outline: none;
-}
-.direction-container .search-location .search-location-svg {
-  position: absolute;
-  left: -30px;
-  top: 10px;
-  color: #bd7ae3;
-}
-.direction-form .direction-container .change-path-svg{
-    position: absolute;
-    right: 10px;
-    top: 35px;
-    color: #6979f8;
-    cursor: pointer;
-}
-.direction-form .direction-container .change-path-svg .change-svg-second{
-    color: #ffffff;
-}
+
 </style>
---
+++ src/views/eggplore-three/EggploreCalendar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="calendar-container">
+    <h5>Date Picker</h5>
+    <div class="calendar-content">
+      <div @click="calendarActive" class="calendar-visible">
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="currentcolor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15 2C15.552 2 16 2.448 16 3V15C16 15.552 15.552 16 15 16H1C0.448 16 0 15.552 0 15V3C0 2.448 0.448 2 1 2H3V0H6V2H10V0H13V2H15ZM14 14V5H2V14H14ZM4 7H6V9H4V7ZM7 7H9V9H7V7Z"
+            fill="currentcolor"
+          />
+        </svg>
+      </div>
+      <div class="calendar-hidden"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "eggplore-calendar",
+  data(){
+    return{
+        isCalendarActive: false
+    }
+  },
+  methods:{
+    calendarActive(){
+        this.isCalendarActive = ! this.isCalendarActive
+    }
+  }
+};
+</script>
+<style>
+.calendar-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 10px 5px 10px;
+  box-sizing: border-box;
+}
+.calendar-container h5 {
+  display: inline-block;
+  margin: 0;
+  font-size: 1rem;
+  text-transform: uppercase;
+  line-height: 24px;
+}
+.calendar-container .calendar-content {
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+.calendar-container .calendar-content .calendar-visible {
+  position: relative;
+  width: 80%;
+  height: 48px;
+  background: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
+  cursor: pointer;
+}
+</style>
