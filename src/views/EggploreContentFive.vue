118e4d7b7d5285d561c1bed2ad3163da3f44ccdf
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-08-16 22:48:23 +0500

    11 change

--- src/views/EggploreContentFive.vue
+++ src/views/EggploreContentFive.vue
@@ -2,7 +2,23 @@
     <div class="eggplore-five-container">
         <div class="eggplore-five-content">
             <eggplore-slider/>
-            <eggplore-tab-bar/>
+            <eggplore-tab-bar :headers="tabHeaders" v-slot="slotProps">
+                <div class="tab-content-container">
+                    <div class="tab-1" v-if="slotProps.active === 'one'">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A illo neque eligendi, et ex aspernatur doloribus, praesentium maxime assumenda autem nobis, asperiores ipsa unde distinctio minima nemo. Commodi eos laborum tempore eius impedit ipsum esse odit sit a, ab facere.
+                    </div>
+                    <div class="tab-2" v-if="slotProps.active === 'two'">
+                    <span v-html="message"></span>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. In ipsum praesentium cupiditate culpa. Doloribus natus iste iure doloremque dolorum dicta ipsa ut voluptatem consectetur eos laborum tempora quibusdam, animi modi aspernatur neque omnis alias culpa totam hic inventore nam delectus odit ratione? Minus iusto rerum alias similique soluta laudantium modi?
+                    </div>
+                    <div class="tab-3" v-if="slotProps.active === 'three'">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure recusandae, necessitatibus placeat ut fugiat a nesciunt sit maiores porro voluptatibus, fuga ipsa, illum aliquid minima cum? Animi reprehenderit ducimus vel?
+                    </div>
+                    <div class="tab-4" v-if="slotProps.active === 'four'">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, iure. Quo cupiditate nesciunt pariatur doloribus alias odio! Reprehenderit, nobis pariatur.
+                    </div>
+                </div>
+            </eggplore-tab-bar>
         </div>
     </div>
 </template>
@@ -11,6 +27,17 @@ import EggploreSlider from './eggplore-five/EggploreSlider.vue';
 import EggploreTabBar from './eggplore-five/EggploreTabBar.vue';
 export default {
     name: 'eggplore-five',
-    components:{EggploreSlider, EggploreTabBar}
+    components:{EggploreSlider, EggploreTabBar},
+    data() {
+        return {
+            message: "<h1>Salom</h1>",
+            tabHeaders: [
+                {name: "Active", value: 'one', active: true},
+                {name: "Inactive", value: 'two', },
+                {name: "Inactive", value: 'three'},
+                {name: "Inactive", value: 'four'}
+            ]
+        }
+    }
 }
 </script>
--- src/views/eggplore-five/EggploreTabBar.vue
+++ src/views/eggplore-five/EggploreTabBar.vue
@@ -2,62 +2,102 @@
   <div class="eggplore-tabbar-container">
     <h5>Tab</h5>
     <div class="tabbar-content">
-      <div class="tabbar" ref="tabChild"></div>
-      <svg
-        width="47"
-        height="3"
-        viewBox="0 0 47 3"
-        fill="currentcolor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 3C0 1.34315 1.34315 0 3 0H44C45.6569 0 47 1.34315 47 3H0Z"
-          fill="currentcolor"
-        />
-      </svg>
+      <div class="tabbar" ref="tabChild">
+        <div v-for="(header, idx) in tabHeaders" :key="idx">
+          <button  :class="{'default-active': header.active}" @click="setActiveTab(header.value)">{{ header.name }}</button>
+          <svg
+            width="47"
+            height="3"
+            viewBox="0 0 47 3"
+            fill="currentcolor"
+            xmlns="http://www.w3.org/2000/svg"
+            v-if="header.active"
+          >
+            <path
+              d="M0 3C0 1.34315 1.34315 0 3 0H44C45.6569 0 47 1.34315 47 3H0Z"
+              fill="currentcolor"
+            />
+          </svg>
+        </div>
+      </div>
+      <slot :active="activeTab" />
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "eggplore-tabbar",
+  props: {
+    headers: {
+      type: Array,
+      default: () => []
+    }
+  },
   mounted() {
-    this.getTabChild();
-    this.addTabClass();
+    // this.getTabChild();
+    // this.addTabClass();
+    this.initHeaders()
+  },
+  data() {
+    return {
+      tabHeaders: []
+    }
+  },
+  computed: {
+    activeTab() {
+      return this.tabHeaders.length > 0 ? this.tabHeaders.find(item => item.active).value : null;
+    }
   },
   methods: {
-    getTabChild() {
-      let tab_child = this.$refs.tabChild;
-      for (let i = 0; i < 4; i++) {
-        const btn = document.createElement("button");
-        tab_child.appendChild(btn);
-        this.addClassButton(btn);
-      }
-      tab_child.firstChild.classList.add("default-active");
-    },
-    addTabClass() {
-      let tab_child = this.$refs.tabChild;
-      if (tab_child.childElementCount) {
-        let childs = tab_child.childNodes;
-        childs.forEach((element) => {
-          if (element.classList.contains("default-active")) {
-            element.innerText = "Active";
-          } else {
-            element.innerText = "InActive";
-          }
-        });
-      }
-    },
-    addClassButton(btn) {
-      btn.addEventListener("click", () => {
-        if (btn.classList.contains("tab-btn-active")) {
-          btn.classList.remove("tab-btn-active");
-        } else {
-          btn.classList.add("tab-btn-active");
+    setActiveTab(tabValue) {
+      this.tabHeaders = this.tabHeaders.map(item => {
+        console.log(item.active)
+        item.active = false;
+
+        if (item.value === tabValue) {
+          item.active = true;
         }
-      });
-      console.log(btn);
+        return item;
+      })
     },
+    initHeaders() {
+      this.tabHeaders = this.headers.map(item => {
+        item.active = !!item.active;
+        return item;
+      })
+    },
+    // getTabChild() {
+    //   let tab_child = this.$refs.tabChild;
+    //   for (let i = 0; i < 4; i++) {
+    //     const btn = document.createElement("button");
+    //     tab_child.appendChild(btn);
+    //     this.addClassButton(btn);
+    //   }
+    //   tab_child.firstChild.classList.add("default-active");
+    // },
+    // addTabClass() {
+    //   let tab_child = this.$refs.tabChild;
+    //   if (tab_child.childElementCount) {
+    //     let childs = tab_child.childNodes;
+    //     childs.forEach((element) => {
+    //       if (element.classList.contains("default-active")) {
+    //         element.innerText = "Active";
+    //       } else {
+    //         element.innerText = "InActive";
+    //       }
+    //     });
+    //   }
+    // },
+    // addClassButton(btn) {
+    //   btn.addEventListener("click", () => {
+    //     if (btn.classList.contains("tab-btn-active")) {
+    //       btn.classList.remove("tab-btn-active");
+    //     } else {
+    //       btn.classList.add("tab-btn-active");
+    //     }
+    //   });
+    //   console.log(btn);
+    // },
   },
 };
 </script>
