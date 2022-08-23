37357369b7851665c4ca64490929679aa114a240
Azizkhan1992 <azizkhannasrullayev1992@gmail.com>
2022-08-23 19:11:56 +0500

    18 change

--- src/views/EggploreContentNine.vue
+++ src/views/EggploreContentNine.vue
@@ -1,28 +1,32 @@
 <template>
-    <div class="carousel-slider-container">
-        <eggplore-carousel-slider :slides="owl_carousel"></eggplore-carousel-slider>
+    <div class="carousel-slider-container" :style="container_style">
+      <div class="carousel-slider-content">
+        <div class="owl-carousel-wrapper" :style="[wrapper_style, transformStep]">
+          <div class="carousel-item" v-for="(slide, idx) in carousel" :key="idx"
+          :style="item_style"
+          >
+            <img :src="require('@/assets/Content-nine/' + slide.img)" alt="">
+            <h3>{{slide.name}}</h3>
+            <p>{{slide.title}}</p>
+            <button>{{slide.button}}</button>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 <script>
-import EggploreCarouselSlider from './eggplore-nine/EggploreCarouselSlider.vue';
+
 export default {
     name: 'carousel-slider',
-    components: {EggploreCarouselSlider},
     data(){
         return{
-            visibleSlide: 0,
+            counter: 0,
+            item_width: 150,
+            margin_right: 15,
+            container_max_width: 825,
             owl_carousel:[
         {
           id: 1,
-          name: "Футбол",
-          img: 'img1.jpg',
-          background_img: 'Vector1.png',
-          title:
-            "Футбо́л (англ. football от foot «ступня» + ball «мяч») — командный вид спорта, в котором целью является забить ...",
-          button: "Подробнее",
-        },
-        {
-          id: 2,
           name: "Футбол",
           img: 'img1.png',
           background_img: 'Vector1.png',
@@ -31,7 +35,7 @@ export default {
           button: "Подробнее",
         },
         {
-          id: 3,
+          id: 2,
           name: "Теннис",
           img: 'img2.png',
           background_img: 'Vector2.png',
@@ -40,7 +44,7 @@ export default {
           button: "Подробнее",
         },
         {
-          id: 4,
+          id: 3,
           name: "Баскетбол",
           img: 'img3.png',
           background_img: 'Vector3.png',
@@ -49,7 +53,7 @@ export default {
           button: "Подробнее",
         },
         {
-          id: 5,
+          id: 4,
           name: "Хоккей",
           img: 'img4.png',
           background_img: 'Vector4.png',
@@ -58,7 +62,7 @@ export default {
           button: "Подробнее",
         },
         {
-          id: 6,
+          id: 5,
           name: "Бокс",
           img: 'img5.png',
           background_img: 'Vector5.png',
@@ -66,17 +70,73 @@ export default {
             "Бокс (от фр. boxe — «бокс» и boxeur — «боксёр», которые происходят от англ. to box — «драться на ринге», «боксировать»...",
           button: "Подробнее",
         },
-        {
-          id: 7,
-          name: "Бокс",
-          img: 'img5.jpg',
-          background_img: 'Vector5.png',
-          title:
-            "Бокс (от фр. boxe — «бокс» и boxeur — «боксёр», которые происходят от англ. to box — «драться на ринге», «боксировать»...",
-          button: "Подробнее",
-        },
       ],
         }
+    },
+    mounted(){
+      this.play()
+      // this.carousel_push()
+    },
+    computed:{
+      item_style(){
+        return {
+          width: `${this.item_width + 'px'}`,
+          marginRight: `${this.margin_right + 'px'}`
+        }
+      },
+      container_style(){
+        return{
+          maxWidth: `${this.container_max_width + 'px'}`
+        }
+      },
+      wrapper_style(){
+        return{
+          width: (this.carousel.length * (this.item_width + this.margin_right)) + 'px'
+        }
+      },
+      transformStep(){
+        const step_size = this.counter * (this.item_width + this.margin_right)
+        return{transform: `translate3d(${-1* step_size}px, 0, 0)`}
+      },
+      left_side(){
+        return JSON.parse(JSON.stringify(this.owl_carousel))
+      },
+      right_side(){
+        return JSON.parse(JSON.stringify(this.owl_carousel))
+      },
+      carousel(){
+        return [...this.left_side, ...this.owl_carousel, ...this.right_side]
+      }
+    },
+    methods:{
+      play(){
+        setInterval(() =>{
+          if(this.counter>= this.carousel.length-2){
+            this.counter = 0
+          }else{
+            this.counter++
+            let item = this.carousel.splice(0,1)
+              for(let i =0; i<item.length; i++){
+                // console.log(item[i])
+              this.carousel.push(item[i])
+              }
+            
+              
+          }
+        }, 5000)
+      },
+      carousel_push(){
+        //TODO
+              // let item = this.carousel.shift()
+              setInterval(()=>{
+                let item = this.carousel.splice(0,1)
+              for(let i =0; i<item.length; i++){
+              this.carousel.push(item[i])
+              }
+              console.log(this.carousel.length, this.counter)
+              },3000)
+        
+      }
     }
 }
 </script>
